f381a3380cbc636763248ac629b4c8867c85fa42
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-06-07 21:22:14 +0300

    [feature #146503613] user api endpoints tests
    Signed-off-by: JULIE WANJA MUGIRA <julie.mugira@andela.com>

---
+++ server/tests/users.spec.js
@@ -0,0 +1,180 @@
+// Change to test environment
+process.env.NODE_ENV = 'test';
+
+// Dev Dependencies
+const server = require('../../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Users', () => {
+  /*
+   * Test the /GET route
+   */
+  describe('/GET user', () => {
+    it('it should GET all users', (done) => {
+      chai.request(server)
+        .get('/api/users')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /GET/:id route
+   */
+  describe('/GET/:id user', () => {
+    it('it should GET a user by the given id', (done) => {
+      chai.request(server)
+        .get('/api/users/2')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('username');
+          res.body.should.have.property('firstName');
+          res.body.should.have.property('secondName');
+          res.body.should.have.property('email');
+          res.body.should.have.property('password');
+          res.body.should.have.property('roleId');
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /PUT/:id route
+   */
+  describe('/PUT/:id user', () => {
+    it('it should not UPDATE a user if user is not an admin or the actual user', (done) => {
+      const user = {
+        email: 'super@man.com',
+      };
+      chai.request(server)
+        .put('/api/users/3')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(403);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('You do not have the permission to make any changes to this user');
+          done();
+        });
+    });
+
+    it('it should not UPDATE a user if user is not found', (done) => {
+      const user = {
+        email: 'super@man.com',
+      };
+      chai.request(server)
+        .put('/api/users/3')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('We could not find this user :(');
+          done();
+        });
+    });
+
+    it('it should UPDATE a user by the given id if an admin', (done) => {
+      const user = {
+        email: 'batman@cave.com',
+      };
+      chai.request(server)
+        .put('/api/users/1')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Your changes have been successfully applied');
+          done();
+        });
+    });
+
+    it('it should UPDATE a user by the given id if id matches current userId', (done) => {
+      const user = {
+        email: 'batman@cave.com',
+      };
+      chai.request(server)
+        .put('/api/users/1')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Your changes have been successfully applied');
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /GET/?limit={integer}?offset={integer} route
+   */
+  describe('/GET/?limit={integer}?offset={integer} for user', () => {
+    it('it should GET users based on query', (done) => {
+      chai.request(server)
+        .get('/api/users/?limit=1&offset=2')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /GET/?username route
+   */
+  describe('/GET/?username search user', () => {
+    it('it should GET a user by the given username', (done) => {
+      chai.request(server)
+        .get('/api/search/users/?username=beauty')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body[0].should.have.property('username').eql('beauty');
+          done();
+        });
+    });
+  });
+
+  /*
+   * Test the /DELETE/:id route
+   */
+  describe('/DELETE/:id user', () => {
+    it('it should not DELETE a user that does not exist', (done) => {
+      chai.request(server)
+        .delete('/api/users/10')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('We could not find this user :(');
+          done();
+        });
+    });
+
+    it('it should not DELETE a user if id does not much current userId or user is not an admin', (done) => {
+      chai.request(server)
+        .delete('/api/users/3')
+        .end((err, res) => {
+          res.should.have.status(403);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('You do not have the permission to make any changes to this user');
+          done();
+        });
+    });
+
+    it('it should DELETE a user given the id if user is an admin', (done) => {
+      chai.request(server)
+        .delete('/api/users/3')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('User successfully deleted');
+          done();
+        });
+    });
+  });
+});
