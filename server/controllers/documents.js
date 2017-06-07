0bef446610ba7a9f2d8990e79f3090dab94a3260
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-06-07 10:36:20 +0300

    [feature #146704647] document controller
    Signed-off-by: JULIE WANJA MUGIRA <julie.mugira@andela.com>

--- server/controllers/documents.js
+++ server/controllers/documents.js
@@ -2,7 +2,7 @@ const Document = require('../models').Document;
 const controllerHelpers = require('../helpers/controllerHelpers');
 
 /* Defines  Document Controller methods */
-class DocumentController {
+class DocController {
 
   /**
     * create method
@@ -11,133 +11,27 @@ class DocumentController {
     * @params res
     * @return { object } - A response to the user
   */
-  create(req, res) {
+  create(req, res) { // route handler
     if (controllerHelpers.validateInput(req.body)) {
-      return res.status(403).json({
+      return res.status(403).json({ // forbidden request
         message: 'Invalid Input',
       });
     }
 
     return Document
-      .create({
+      .create({ // request
         title: req.body.title,
         content: req.body.content,
         access: req.body.access || 'private',
         userId: req.user.id,
       })
-      .then((document) => {
-        res.status(200).json({
-          message: 'You have successfuly created a document',
+      .then((document) => { // response
+        res.status(200).json({ // request was successful
+          message: 'Document created successfully!',
           document,
         });
       })
       .catch(error => res.status(400).json(error));
-  }
-
-   /**
-    * list method
-    * Lists all documents found in the database
-    * @params req
-    * @params res
-    * @return { object } - A response to the user
-  */
-  list(req, res) {
-    const privateDocs = req.data;
-    if (req.query.limit || req.query.offset) {
-      return Document
-        .findAll({
-          limit: req.query.limit,
-          offset: req.query.offset,
-        })
-        .then((document) => {
-          if (!document) {
-            return res.status(404).json({
-              message: 'We could not find this document :(',
-            });
-          }
-          res.status(200).json(document);
-        })
-        .catch((error) => {
-          res.status(400).json(error);
-        });
-    }
-    return Document
-      .findAll({
-        where: {
-          access: 'public',
-        },
-      })
-      .then(document => res.status(200).json({ document, privateDocs }))
-      .catch(error => res.status(400).json(error));
-  }
-
-   /**
-    * find method
-    * Finds a document by the id specified
-    * @params req - document id
-    * @params res
-    * @return { object } - A response to the user
-  */
-  find(req, res) {
-    return Document
-      .findById(req.params.id)
-      .then((document) => {
-        if (!document) {
-          return res.status(404).json({
-            message: 'We could not find this document :(',
-          });
-        }
-        return res.status(200).json(document);
-      })
-      .catch(error => res.status(400).json(error));
-  }
-
-   /**
-    * update method
-    * Updates a document
-    * @params req - document id, change to be made
-    * @params res
-    * @return { object } - A response to the user
-  */
-  update(req, res) {
-    if (controllerHelpers.validateInput(req.body)) {
-      return res.status(403).json({
-        message: 'Invalid Input',
-      });
-    }
-    const updateData = req.body;
-    return Document
-      .update(updateData, {
-        where: {
-          id: req.params.id,
-        },
-      })
-      .then(() => res.status(200).json({
-        message: 'Your changes have been successfully applied',
-      }))
-      .catch((error) => {
-        res.status(400).json(error);
-      });
-  }
-
-   /**
-    * delete method
-    * Deletes a document
-    * @params req - document id
-    * @params res
-    * @return { object } - A response to the user
-  */
-  delete(req, res) {
-    return Document
-      .destroy({
-        where: {
-          id: req.params.id,
-        },
-      })
-      .then(() => res.status(200).json({ message: 'Document successfully deleted' }))
-      .catch((error) => {
-        res.status(400).json(error);
-      });
   }
 
    /**
@@ -166,6 +60,112 @@ class DocumentController {
         res.status(400).json(error);
       });
   }
+
+   /**
+    * find method
+    * Finds a document by the id specified
+    * @params req - document id
+    * @params res
+    * @return { object } - A response to the user
+  */
+  find(req, res) {
+    return Document
+      .findById(req.params.id)
+      .then((document) => {
+        if (!document) {
+          return res.status(404).json({
+            message: 'document not available',
+          });
+        }
+        return res.status(200).json(document);
+      })
+      .catch(error => res.status(400).json(error));
+  }
+
+   /**
+    * list method
+    * Lists all documents found in the database
+    * @params req
+    * @params res
+    * @return { object } - A response to the user
+  */
+  list(req, res) {
+    const privateDocs = req.data;
+    if (req.query.limit || req.query.offset) { // to restrict the number of rows
+      return Document
+        .findAll({
+          limit: req.query.limit,
+          offset: req.query.offset,
+        })
+        .then((document) => {
+          if (!document) {
+            return res.status(404).json({
+              message: 'Document not available',
+            });
+          }
+          res.status(200).json(document);
+        })
+        .catch((error) => {
+          res.status(400).json(error);
+        });
+    }
+    return Document
+      .findAll({
+        where: {
+          access: 'public',
+        },
+      })
+      .then(document => res.status(200).json({ document, privateDocs }))
+      .catch(error => res.status(400).json(error));
+  }
+
+   /**
+    * update method
+    * Updates a document
+    * @params req - document id, change to be made
+    * @params res
+    * @return { object } - A response to the user
+  */
+  update(req, res) {
+    if (controllerHelpers.validateInput(req.body)) {
+      return res.status(403).json({
+        message: 'Invalid Input',
+      });
+    }
+    const updateData = req.body;
+    return Document
+      .update(updateData, {
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(() => res.status(200).json({
+        message: 'Successfully updated!',
+      }))
+      .catch((error) => {
+        res.status(400).json(error);
+      });
+  }
+
+   /**
+    * delete method
+    * Deletes a document
+    * @params req - document id
+    * @params res
+    * @return { object } - A response to the user
+  */
+  delete(req, res) {
+    return Document
+      .destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(() => res.status(200).json({ message: 'Document successfully deleted' }))
+      .catch((error) => {
+        res.status(400).json(error);
+      });
+  }
 }
 
-module.exports = new DocumentController();
+module.exports = new DocController();
