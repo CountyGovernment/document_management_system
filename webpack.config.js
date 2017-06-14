eb4fd264598ce2c30b1f1509d90de9fe43ffafb9
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-06-14 09:10:22 +0300

    [feature #146368967] set-up folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: path.resolve(__dirname, 'client/index.html'),
   filename: 'index.html',
   inject: 'body',
 });
 
 module.exports = {
-  entry: './client/index.jsx',
+  entry: path.resolve(__dirname, './client/index.jsx'),
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js',
